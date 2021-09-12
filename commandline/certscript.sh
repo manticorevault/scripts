ddfcbf1ebe798c4ef0e01c984e4091a1db64f8c9
manticorevault <artur_seilah@hotmail.com>
2021-09-12 16:20:43 -0300

    Refactored and corrected some small mistakes in the scripts

--- commandline/certscript.sh
+++ commandline/certscript.sh
@@ -3,7 +3,7 @@
 # The first line of output from the script should consist of the
 # name of the user who invoked it.
 echo You are:
- whoami
+whoami
 
 #The second line of output should contain the IP address
 #of the default gateway.
